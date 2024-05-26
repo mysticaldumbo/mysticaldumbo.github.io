cb46ba69f2b7a02049562dc7e50d0e63705953a1
copper <bananaananab69@gmail.com>
2024-05-26 10:18:03 -0500

    add more to stat view.

--- scripts/statview.js
+++ scripts/statview.js
@@ -1,11 +1,17 @@
 function showStats() {
-    const username = document.getElementById('username').value;
+    let username = document.getElementById('username').value;
     if (username) {
-        const statsDiv = document.getElementById('stats');
-        const imgUrl = `https://github-readme-stats.vercel.app/api/top-langs?username=${username}&show_icons=true&theme=tokyonight&layout=compact`;
-        statsDiv.innerHTML = `<img src="${imgUrl}" alt="GitHub Stats for ${username}">`;
-        document.getElementById('copyLink').setAttribute('data-url', imgUrl);
+        let statsDiv = document.getElementById('stats');
+        let generalStatsUrl = `https://github-readme-stats.vercel.app/api?username=${username}&theme=tokyonight&show_icons=true&hide_border=true&count_private=true`;
+        let langStatsUrl = `https://github-readme-stats.vercel.app/api/top-langs?username=${username}&show_icons=true&theme=tokyonight&layout=compact`;
+        let streakStatsUrl = `https://github-readme-streak-stats.herokuapp.com/?user=${username}&theme=tokyonight&hide_border=true`;
+
+        statsDiv.innerHTML = `
+            <img src="${generalStatsUrl}" alt="github stats for ${username}">
+            <img src="${langStatsUrl}" alt="top languages for ${username}">
+            <img src="${streakStatsUrl}" alt="github streak stats for ${username}">
+        `;
     } else {
-        alert('please enter a github username');
+        alert('please enter a github username.');
     }
 }
